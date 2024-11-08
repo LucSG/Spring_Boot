c530cb804ef082aa6c4ac3fb588a89986a74bb47
Lucas Santos Guimarães <lucasguima@yahoo.com.br>
2024-11-08 15:25:08 -0500

    Customer
    Criação basica de um crud, com erro no mysql, procurando o problema no meu computador

---
+++ src/main/java/com/schoolofnet/CustomerController.java
@@ -0,0 +1,37 @@
+package com.schoolofnet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.schoolofnet.entity.Customer;
+import com.schoolofnet.entity.repository.CustomerRepository;
+
+@Controller
+
+@RequestMapping(path = "/customers")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerRepository repository;
+	
+	public CustomerController(CustomerRepository customerRepository) {
+		this.repository = customerRepository;
+	}
+	
+	@PostMapping
+	@ResponseBody
+	
+	public Customer create(@RequestParam("name") String name, @RequestParam("age") Integer age) {
+		Customer customer = new Customer(name,age);
+		
+		repository.save(customer);
+		
+		
+		return customer;
+	}
+
+}
