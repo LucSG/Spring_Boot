91fb278b405a73411151a91b82b355f5a47de9a0
Lucas Santos Guimarães <lucasguima@yahoo.com.br>
2024-11-13 14:51:08 -0500

    Comando DELETE + formatação
    criação do comando delete do bd e mais um pouco de organização de código

--- src/main/java/com/schoolofnet/CustomerController.java
+++ src/main/java/com/schoolofnet/CustomerController.java
@@ -1,15 +1,20 @@
 package com.schoolofnet;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import com.schoolofnet.entity.Customer;
 import com.schoolofnet.entity.repository.CustomerRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/customers")
@@ -36,6 +41,11 @@ public class CustomerController {
 			return ResponseEntity.notFound().build();
 		}
 	}
+	
+	@GetMapping(path = "/name/{name}")
+	public Customer findByName(@PathVariable("name") String name) {
+		return repository.findByName(name);
+	}
 
 	@PostMapping
 	public ResponseEntity<Customer> create(@RequestParam("name") String name, @RequestParam("age") Integer age) {
@@ -45,5 +55,16 @@ public class CustomerController {
 		Customer customer = new Customer(name, age);
 		repository.save(customer);
 		return new ResponseEntity<>(customer, HttpStatus.CREATED);
+
+	
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void destroy(@PathVariable("id") Long id) {
+		Optional<Customer> customer = repository.findById(id);
+		
+		if(customer.isPresent()) {
+			repository.delete(customer.get());
+		}
 	}
 }
